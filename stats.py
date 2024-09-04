f3cbf8949c86c644f3a65f9db6406c21b648ee14
Viktor Davydov <davydov.viktor.andreevich@gmail.com>
2024-09-04 18:09:24 +0300

    plots func process

--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 class Tables:
@@ -26,26 +27,58 @@ class Tables:
     def to_excel(self, dataframe) -> None:
         dir_name = 'Reports'
         if not os.path.exists(dir_name):
-            os.mkdir('Reports')
+            os.mkdir(dir_name)
         else:
             pass
         writer = pd.ExcelWriter(f'{dir_name}/1.xlsx', engine='xlsxwriter')
         dataframe.to_excel(writer, sheet_name='Детализация', index=False)
         writer._save()
         
-class Plot:
+class Plot:        
     
-    # def __init__(self, array_1: list, array_2: list):
-    #     self.array_1 = array_1
-    #     self.array_2 = array_2
-    
-    # def make_arrays_for_plot(self, sorted_tags_list: list[dict]) -> list:
+    def make_plot(self, arrays_dict: dict):
         
-    
-    def make_plot(self, service_name: str, week: list[int], issues: list[int]):
-        plt.plot(week, issues, marker='o')
-        plt.xlabel('Неделя')
-        plt.ylabel('Кол-во ошибок, шт.')
-        plt.title(service_name)
-        plt.show()
+        for service_name, arrays in arrays_dict.items():
+            plt.figure(figsize=(16,5))
+            plt.title(service_name)
+            
+            # График ошибок Код (Backend/Frontend)
+            plt.subplot(1, 3, 1)
+            x = np.array(arrays["code_week_array"])
+            y = np.array(arrays["code_issues_array"])
+            plt.xlabel("Недели\nКод (Backend/Frontend)")
+            plt.ylabel("Кол-во ошибок, шт.")
+            plt.xticks(arrays["code_week_array"])
+            plt.yticks(arrays["code_issues_array"])
+            plt.plot(x, y, marker = "o")
+            
+            # График ошибок "Внутренняя (системная)"
+            plt.subplot(1, 3, 2)
+            x = np.array(arrays["inner_week_array"])
+            y = np.array(arrays["inner_issues_array"])
+            plt.xlabel("Недели\nВнутренняя (системная)")
+            plt.ylabel("Кол-во ошибок, шт.")
+            plt.xticks(arrays["inner_week_array"])
+            plt.yticks(arrays["inner_issues_array"])
+            plt.plot(x, y, marker = "o")
+            
+            # График ошибок "Внешняя (сайт/источник/вендор)"
+            plt.subplot(1, 3, 3)
+            x = np.array(arrays["outer_week_array"])
+            y = np.array(arrays["outer_issues_array"])
+            plt.xlabel("Недели\nВнешняя (сайт/источник/вендор)")
+            plt.ylabel("Кол-во ошибок, шт.")
+            plt.xticks(arrays["outer_week_array"])
+            plt.yticks(arrays["outer_issues_array"])
+            plt.plot(x, y, marker = "o")
+            
+            plt.show()
+        
+        # dir_name = 'Plots'
+        # if not os.path.exists(dir_name):
+        #     os.mkdir(dir_name)
+        # else:
+        #     pass
+        # plt.savefig(f"{dir_name}/fig_1")
+        
         
