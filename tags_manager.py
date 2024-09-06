62bd88a6e5953b8c521a2201905be08a2c053000
Viktor Davydov <davydov.viktor.andreevich@gmail.com>
2024-09-06 18:53:09 +0300

    week array logic fix

--- tags_manager.py
+++ tags_manager.py
@@ -1,4 +1,4 @@
-
+import datetime
 
 class TagsManager:
     
@@ -28,31 +28,25 @@ class TagsManager:
             stats_dict[k] = result
         return stats_dict
     
-    def make_arrays_for_plots(self, formated_tags: dict) -> dict:
+    def make_arrays_for_plots(self, formated_tags: dict, start_date: str, end_date: str) -> dict:
         '''
         Создание arrays для графиков
         '''
         arrays_for_plots = {}
         longest_week_list = []
         
+        # Генерация списка недель
         
-        # Выявление наиболее длинного списка недель
+        start_week = datetime.datetime.strptime(start_date, '%d.%m.%Y')
+        end_week = datetime.datetime.strptime(end_date, '%d.%m.%Y')
+        weeks_list = [start_week.isocalendar()[1], end_week.isocalendar()[1]]
         
-        for issues in formated_tags.values():
-            
-            code_issues_list = issues.get("code")
-            inner_issues_list = issues.get("inner")
-            outer_issues_list = issues.get("outer")
-            
-            code_week_array = [item.get("week") for item in code_issues_list]
-            inner_week_array = [item.get("week") for item in inner_issues_list]
-            outer_week_array = [item.get("week") for item in outer_issues_list]
-
-            longest_week_list += code_week_array + inner_week_array + outer_week_array
-            
-        longest_week_list = sorted(list(set(longest_week_list)))
-        print(longest_week_list)
+        week_delta = weeks_list[-1] - weeks_list[0]
         
+        for week in range(1, week_delta):
+            weeks_list.append(weeks_list[0] + week)
+        
+        longest_week_list = sorted((weeks_list))
         
         # Сбор списков ошибок длиной равному наиболее длинному списку недель
                 
